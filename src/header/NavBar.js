78640f245f40c7a763b2a8df4fd52c6c9b106310
Brooke <Brooke.horrocks7@gmail.com>
2019-02-12 22:17:24 -0700

    class name and minor style correction

--- src/header/NavBar.js
+++ src/header/NavBar.js
@@ -22,9 +22,9 @@ function NavBar() {
                 </Button>
             </div>
             <div className="page-navbar">
-                <Link class="link" to="/art">PORTFOLIO</Link>
-                <Link class="link" to="/bio">BIO</Link>
-                <Link class="link" to="/contact">CONTACT</Link>
+                <Link className="link" to="/art">PORTFOLIO</Link>
+                <Link className="link" to="/bio">BIO</Link>
+                <Link className="link" to="/contact">CONTACT</Link>
             </div>
         </nav>
     )
